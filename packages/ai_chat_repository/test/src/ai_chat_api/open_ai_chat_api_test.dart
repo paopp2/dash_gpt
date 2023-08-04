595619e8f8f844c56facf9f2a6577f639a9c1541
Paolo Pepito <npaolopepito@gmail.com>
2023-08-05 00:28:26 +0800

    Add OpenAIChatAPI test outline

---
+++ packages/ai_chat_repository/test/src/ai_chat_api/open_ai_chat_api_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('OpenAIChatApi', () {
+    group('chatRoomStream', () {});
+    group('createNewChatRoom', () {});
+    group('getChatRoomHeaders', () {});
+    group('sendMessage', () {});
+    group('updateChatRoom', () {});
+    group('toOpenAIMessage', () {});
+    group('getAIReplyMessageStream', () {});
+  });
+}
